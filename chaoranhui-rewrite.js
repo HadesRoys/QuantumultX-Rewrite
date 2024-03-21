b539514fe957d418b56f46fd12eaf85a1caac895
HadesRoys <33996199+HadesRoys@users.noreply.github.com>
2024-03-21 12:19:36 +0800

    Update chaoranhui-rewrite.js

--- chaoranhui-rewrite.js
+++ chaoranhui-rewrite.js
@@ -22,7 +22,7 @@ var body = $response.body;
 
 var obj = JSON.parse(body);
 
-obj.resultData.PRODUCT_NUM = 100;
+// obj.resultData.PRODUCT_NUM = 100;
 obj.resultData.EXCHANGE_COUNT = 0;
 
 var newBody = JSON.stringify(obj);
